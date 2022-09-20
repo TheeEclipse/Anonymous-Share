c914a2a486a68e74a7272d154f13746ceb994d6c
Kelvin Ndungu Wanja <wanjakelvinndungu@gmail.com>
2022-09-20 15:55:06 +0300

    Update register.php

--- register.php
+++ register.php
@@ -5,28 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
     <title>Anonymous Ecorp</title>
-    <meta name="desciption" content="Anonymous Ecorp. Share Anonymously across the world. Fast, Simple, Secure and uses latest technology  ensuring you leave no digital footprints. Just like you were never here!!">
-    <link rel="icon" type="image/x-icon" href="alogo.png">
-    <meta name="description" content="Anonymous Share">
-    <meta property="og:image"  content="https://res.cloudinary.com/dyadjj49h/image/upload/v1661000623/g2_v1fu4s.png">
-    <meta property="og:image:width" content="300">
-    <meta property="og:image:height" content="300">
-    <meta property="og:description" content="Share secret messages with friends,lovers or associates globally">
-    <meta property="og:url"content="https://anonymous.astralecorp.com" >
-    <meta property="og:title" content="Anonymous Share">
-    <meta name="twitter:site" content="@astralecorp" >
-    <meta name="twitter:description" content="Anonymous Share" >
-    <meta name="twitter:title" content="Anonymous Ecorp" >
-    <meta name="twitter:creator" content="@astralecorp" >
-    <meta name="twitter:url" content="https://anonymous.astralecorp.com" >
-    <meta name="twitter:image" content="https://res.cloudinary.com/dyadjj49h/image/upload/v1661000623/g2_v1fu4s.pngg" >
-    <meta name="theme-color" content="#9900cc" >
-    <!--bootstrap-->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!--jquery-->
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link rel="stylesheet" href="https://res.cloudinary.com/dyadjj49h/raw/upload/v1661183645/stylesapi_ocy1jrAnonymous22Aug_rmwhva.css">
+ <!--import jquery and necessary meta tags ,styles and bootstrap-->
 </head>
 <body>
 <form method="post" action="Sign up.php" id="form2">
